a5f0108f8a35ef298c040704675355c36a1ae202
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-17 18:34:34 +0300

    prctice with styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+
+// const Button = styled.button`
+//   background: green;
+//   color: white;
+//   margin: 5% 5%;
+// `;
+
+// const Container = styled.div`
+//   background: green;
+//   color: red;
+
+//   .hero {
+//     color: orange;
+//     font-size: 4rem;
+//     text-transform: uppercase;
+//   }
+// `;
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <div>
+      comfy sloth starter
+      <Button>Click me</Button>
+      <Container>
+        <div>
+          <h3>Hi!</h3>
+        </div>
+        <div className="hero">
+          <p>Hero text</p>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default App
+export default App;
