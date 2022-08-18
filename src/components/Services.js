a56bcd1351a3eb97f7a73df6cda3663ffd848d29
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-18 22:21:54 +0300

    services setup

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import React from "react";
+import styled from "styled-components";
+import { services } from "../utils/constants";
 
 const Services = () => {
-  return <h4>services </h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            custom furniture <br />
+            built only for you
+          </h3>
+          <p>
+            {" "}
+            Sit asperiores illo alias dolorum doloribus consequuntur molestiae
+            iusto sapiente eaque. Ipsam esse animi commodi quae nihil
+            voluptatem. Neque repellendus magni alias mollitia voluptatem amet
+            dolor nemo nobis ipsam delectus non et sit, ipsum dolorum natus
+            impedit, quam blanditiis? Sint fuga corporis, nobis molestiae totam
+            ab quia cumque vel enim repudiandae!
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const { id, icon, title, text } = service;
+            return (
+              <article key={id} className="service">
+                <span className="icon">{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   h3,
@@ -68,5 +100,5 @@ const Wrapper = styled.section`
       transform: translateY(5rem);
     }
   }
-`
-export default Services
+`;
+export default Services;
