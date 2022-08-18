3351c97819257674f5cf0e86e2721da0d3316892
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-18 22:33:49 +0300

    contact form setup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Ducimus similique, quos at exercitationem adipisci praesentium
+            molestias natus eum non, corporis necessitatibus pariatur officia.
+            Sit asperiores illo alias dolorum doloribus.
+          </p>
+          <form
+            action="submitt"
+            className="contact-form"
+            onSubmit={console.log(1)}
+          >
+            <input
+              type="email"
+              className="form-input"
+              placeholder="enter email"
+            />
+            <button type="submit" className="submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +94,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
